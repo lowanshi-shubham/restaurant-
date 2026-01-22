b1b68eb1a9ccfcebbfe5c2b02cc8026e4becf488
shubham <shubhamlowanshi12345@gmail.com>
2026-01-22 18:29:30 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -28,9 +28,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-  res.send("Hello from server");
-});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/menu", menuRoutes);
