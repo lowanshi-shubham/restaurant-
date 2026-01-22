9d38e6b5dc5624d20c89093014a7b88810a8daf0
shubham <shubhamlowanshi12345@gmail.com>
2026-01-22 16:20:56 +0530

    fix refresh problem

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -18,8 +19,10 @@ connectCloudinary();
 // middlewares
 app.use(express.json());
 app.use(cors({
-  origin: "https://restaurant-hsgw.onrender.com",   // ✅ Sirf frontend allow
-  methods: ["GET", "POST", "PUT", "DELETE" ,"PATCH"],
+ origin: [
+   "https://restaurant-hsgw.onrender.com",
+    "http://localhost:5173"
+  ],  methods: ["GET", "POST", "PUT", "DELETE" ,"PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
@@ -34,6 +37,18 @@ app.use("/api/menu", menuRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/booking", bookingRoutes);
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.use((req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend/dist/index.html")
+  );
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
